13c61b67fcc10a4e9f2a5825fd2fa6a6075b7a64
NDook <thomasmludek@gmail.com>
2022-05-30 14:16:14 +0200

    create

--- index.js
+++ index.js
@@ -79,29 +79,29 @@ const player = new Player(canvas.width / 2, canvas.height / 2, 10, "yellow");
 player.draw();
 
 const projectiles = [];
-    window.addEventListener("click", (event) => {
-        const angle = Math.atan2(
-            event.clientY - player.y,
-            event.clientX - player.x
-        );
-        const velocity = {
-            x: Math.cos(angle) * 5,
-            y: Math.sin(angle) * 5,
-        };
+const enemies = [];
+const particles = [];
 
-        const projectile = new Projectile(
-            player.x,
-            player.y,
-            5,
-            "white",
-            velocity
-        );
+window.addEventListener("click", (event) => {
+    const angle = Math.atan2(
+        event.clientY - player.y,
+        event.clientX - player.x
+    );
+    const velocity = {
+        x: Math.cos(angle) * 5,
+        y: Math.sin(angle) * 5,
+    };
+
+    const projectile = new Projectile(
+        player.x,
+        player.y,
+        5,
+        "white",
+        velocity
+    );
     projectile.draw();
     projectiles.push(projectile);
 });
-
-const enemies = [];
-const particles = [];
 
 let animationId;
 function animate() {
